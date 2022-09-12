26b95fdc44af0534847b705630e2f56769d0d002
DESKTOP-46I2GAU\nguoi <haodcdng@gmail.com>
2022-09-12 22:18:49 +0700

    1st

---
+++ web/config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'general' => [
+        'branch_name' => 'Kiddy Intelligent',
+        'site_name' => 'Kiddy Intelligent',
+        'site_name_upper_1' => 'Kiddy Intelligent',
+	    'site_name_upper_2' => 'Kiddy Intelligent',
+        'slogan' => 'Intelligent game for Kiddy',
+        'per_page' => '5'
+    ],
+    'link' => [
+        'site_no_lang' => 'localhost:8000',
+        'schema' => 'http://'
+    ]
+]
+
+?>
